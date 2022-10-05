4933c54e7509dee5919c3f639ae1e5c8b432d779
Hanyue_Guo <moon.guo@mail.utoronto.ca>
2022-10-04 23:56:43 -0400

    modified Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,45 +1,57 @@
-class Polynomial {
-	//(I) Array of double
-	double coef[] = new double[10];
+public class Polynomial {
+  
+	double coef[] = new double[10]; //only non-zero coefficients
+  	int expo[] = new int[10]; //corresponding 
 		
-	//(II) No-argument constructor that sets the polynomial to zero 
 	Polynomial(){
 		for (int i = 0; i < 10; i++){
 			coef[i] = 0.0;
+          	expo[i] = 0;
 		}
 	}
 
-	//(III) Constructor that takes an array of double as an argument and sets the coefficients accordingly
-	Polynomial(double[] newcoef){
+	Polynomial(double[] newcoef, int[] newexpo){
 		for (int i = 0; i < newcoef.length; i++){
 			coef[i] = newcoef[i];
+          	expo[i] = newexpo[i];
 		}
 	}
 
-	//(IV) It has a method named add that takes one argument of type Polynomial and returns the polynomial resulting from adding the calling object and the argument
+  
 	Polynomial add(Polynomial newpoly) {
-      	double[] finalpoly = new double[10];
+      	double[] finalcoef = new double[10];
+      	int[] finalexpo = new int[10];
+      
+ 
 		for (int i = 0; i < 10; i++){
-			finalpoly[i] = coef[i] + newpoly.coef[i];
+			finalcoef[i] = coef[i] + newpoly.coef[i];
+          	finalexpo[i] = expo[i] + newpoly.expo[i];
 		}
-		return new Polynomial(finalpoly);
+		return new Polynomial(finalcoef, finalexpo);
 	}
+  	
+  	boolean contains(int x, int[] array){
+      for(int i = 0; i < 10; i++){
+        if(array[i] == x)
+          return true;
+      }
+      return false;
+    }
 
-	//(V) It has a method named evaluate that takes one argument of type double representing a value of x and evaluates the polynomial accordingly.
+
 	double evaluate(double X){
 		double y = 0;
 		for (int i = 0; i < 10; i++){
-			y += coef[i] * Math.pow(X, i);
+			y += coef[i] * Math.pow(X, expo[i]);
 		}
 		return y;
 	}
 
-	//It has a method named hasRoot that takes one argument of type double and determines whether this value is a root of the polynomial or not. N
-	boolean hasRoot(double Y){
-		double finalY = 0;
+	boolean hasRoot(double X){
+		double y = 0;
 		for (int i = 0; i < 10; i++){
-			finalY += coef[i] * Math.pow(Y, i);
+			y += coef[i] * Math.pow(X, expo[i]);
 		}
-		return (finalY == 0);
+		return (y == 0);
 	}
 }
